f590f2a7dd68ee59c1c92a7709c246833c17afd6
unknown <richard@vanirinstalledsales.com>
2025-07-09 10:07:51 -0400

    p

--- script.js
+++ script.js
@@ -7,8 +7,7 @@ let tsvContent = `SKU\tDescription\tDescription 2\tUOM\tQTY\tColor Group\n`;
 let allSelected = false;
 let toggleButton;
 
-const baseServer = "https://5b3dfcf691d7.ngrok-free.app";
-
+const baseServer = " https://ca67c17416e5.ngrok-free.app";
 const defaultServer = `${baseServer}/inject`;
 const savedServer = localStorage.getItem("injectionServerURL");
 const serverURL = savedServer || defaultServer;
