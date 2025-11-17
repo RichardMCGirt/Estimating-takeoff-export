a5519d19d31191a037a2ca2afdc0001b33f227a2
RichardMCGirt <richard.mcgirt@vanirinstalledsales.com>
2025-11-17 07:40:20 -0500

    Update script.js

--- Scripts/script.js
+++ Scripts/script.js
@@ -9,7 +9,7 @@ let toggleButton;
 let skuLookup = new Map(); 
 
 const BREAKOUT_GROUP_BY = "desc2+sku"; 
-const baseServer = "https://120bd0dce2af.ngrok-free.app"
+const baseServer = "https://cc2ba6d432a7.ngrok-free.app"
 const defaultServer = `${baseServer}/inject`;
 const savedServer = localStorage.getItem("injectionServerURL");
 const serverURL = savedServer || defaultServer;
